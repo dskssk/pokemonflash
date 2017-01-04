d8721d27916f45554a929536b31d86ca4fb082c3
dskssk <dsk.ssk@gmail.com>
2017-01-04 22:59:42 +0900

    jQueryを使うように変更

--- pokemonflash.ts
+++ pokemonflash.ts
@@ -1,10 +1,10 @@
-window.addEventListener('DOMContentLoaded', function() {
+$(function(){
   const FPS = 1;
-  const target: HTMLElement[] = [].slice.call(document.querySelectorAll('.pokemonflash'));
+  const target = $('.pokemonflash');
   if (target.length === 0) return;
   window.setInterval(() => {
     const oldColor = target[0].style.backgroundColor; 
     const newColor = oldColor === 'red' ? 'blue' : 'red';
-    target.forEach(v => v.style.backgroundColor = newColor);
+    target.css('background-color', newColor);
   }, 1000 / FPS);
 });
