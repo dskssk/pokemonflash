1dc4d8ae2874c9faf855e29781d908be0d762835
dskssk <dsk.ssk@gmail.com>
2017-01-04 23:06:49 +0900

    1. jQueryロード先をCDNからローカルに修正 2. 初回の色変更も待ってしまっていたので、初回だけは直後に呼び出すよう修正

--- pokemonflash.ts
+++ pokemonflash.ts
@@ -2,9 +2,11 @@ $(function(){
   const FPS = 1;
   const target = $('.pokemonflash');
   if (target.length === 0) return;
-  window.setInterval(() => {
+  const switchColor = () => {
     const oldColor = target[0].style.backgroundColor; 
     const newColor = oldColor === 'red' ? 'blue' : 'red';
     target.css('background-color', newColor);
-  }, 1000 / FPS);
+  };
+  switchColor();
+  window.setInterval(switchColor, 1000 / FPS);
 });
